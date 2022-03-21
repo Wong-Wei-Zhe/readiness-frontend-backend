22320a68dda0e2c0a795f851f85a4a3f36b26fda
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2022-03-21 14:32:20 +0000

    alb1 deploy

--- server/server.js
+++ server/server.js
@@ -7,13 +7,33 @@ const bodyParser = require("body-parser");
 const uri = process.env.MONGO_URI;
 console.log(uri);
 const app = express();
-app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  let allowedOrigins = [
+    "http://18.140.198.84:8080",
+    "http://ec2-18-140-198-84.ap-southeast-1.compute.amazonaws.com:8080",
+    "http://18.141.161.10:8080",
+    "http://ec2-18-141-161-10.ap-southeast-1.compute.amazonaws.com:8080",
+  ];
+  let origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  //res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin , Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+
+  next();
+});
 
 var forex = require("./routes/forex");
 const watchlist = require("./routes/watchlist");
