e30c9b8f3c89e49cdfc383fb1e01f1658f27ec89
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2022-03-21 12:47:30 +0000

    Added config files

---
+++ client/src/configs/FirebaseConfig.js
@@ -0,0 +1,21 @@
+const FirebaseConfig = {
+  apiKey: "AIzaSyCul7R9BUWqXQpDadxLPy7wFEmnapAD0mU",
+  authDomain: "trexservices-5fe1c.firebaseapp.com",
+  projectId: "trexservices-5fe1c",
+  storageBucket: "trexservices-5fe1c.appspot.com",
+  messagingSenderId: "540922506377",
+  appId: "1:540922506377:web:b6851e9a471df5cc022a58",
+};
+
+// const FirebaseConfig = {
+//   apiKey: process.env.REACT_APP_FB_APIKEY,
+//   authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_FB_PROJECTID,
+//   storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_FB_APPID,
+// };
+
+export default FirebaseConfig;
+
+// docker build --build-arg REACT_APP_ENDPOINT=http://api.com -t trexwebapp .
